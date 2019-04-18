61813085242b8f19f4c6f9f95b60080d407ac312
spiritxhx <spiritxhx@gmail.com>
2019-04-18 16:18:16 -0400

    create Business Component

--- first-try/src/components/BusinessList/BusinessList.js
+++ first-try/src/components/BusinessList/BusinessList.js
@@ -2,4 +2,8 @@ import React from 'react';
 import './Business.css';
 import Business from '../Business/Business';
 
-
+class BusinessList extends React.Component {
+    render() {
+        
+    }
+}
