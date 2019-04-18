cf3a12aa01d61e837f9a981fdeeb014240dc71ad
spiritxhx <spiritxhx@gmail.com>
2019-04-18 17:06:30 -0400

    render the searchbar

--- first-try/src/components/SearchBar/SearchBar.js
+++ first-try/src/components/SearchBar/SearchBar.js
@@ -16,4 +16,22 @@ class SearchBar extends React.Component {
             );
         });
     }
+    render() {
+        return(
+            <div className="SearchBar">
+                <div className="SearchBar-sort-options">
+                    <ul>
+                    {renderSortByOptions()}
+                    </ul>
+                </div>
+                <div className="SearchBar-fields">
+                    <input placeholder="Search Businesses" />
+                    <input placeholder="Where?" />
+                </div>
+                <div className="SearchBar-submit">
+                    <a>Let's Go</a>
+                </div>
+            </div>
+        );
+    }
 }
