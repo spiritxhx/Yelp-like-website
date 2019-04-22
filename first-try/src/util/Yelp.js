15d6afa41b5fa70cb87f8fed374b5d9037a27496
spiritxhx <spiritxhx@gmail.com>
2019-04-22 18:49:50 -0400

    add search 2

--- first-try/src/util/Yelp.js
+++ first-try/src/util/Yelp.js
@@ -5,5 +5,13 @@ const Yelp={
 };
 
 const search = (term, location, sortBy) =>{
-    return fetch();
+    return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, 
+        {
+            headers: {
+                Authorization: `Bearer ${apiKey}`
+            }
+        }
+    ).then (response=>{
+        return response.json();
+    });
 };
