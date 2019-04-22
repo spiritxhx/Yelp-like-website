2473501ed5b90fc76f4033492ce947e0f1d0c823
spiritxhx <spiritxhx@gmail.com>
2019-04-22 19:02:57 -0400

    import yelp.js

--- first-try/src/App.js
+++ first-try/src/App.js
@@ -2,9 +2,11 @@ import React, { Component } from 'react';
 import './App.css';
 import BusinessList from './components/BusinessList/BusinessList';
 import SearchBar from './components/SearchBar/SearchBar';
+import Yelp from './util/Yelp';
 
 const business = {
   imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+  id: '',
   name: 'MarginOtto Pizzeria',
   address: '1010 Paddington Way',
   city: 'Flavortown',
