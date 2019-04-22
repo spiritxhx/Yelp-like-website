2092d0aef4f26e749cb06bd97af04a45f1011197
spiritxhx <spiritxhx@gmail.com>
2019-04-22 19:58:11 -0400

    bug fix attempt 1

--- first-try/src/util/Yelp.js
+++ first-try/src/util/Yelp.js
@@ -1,8 +1,10 @@
+import App from "../App";
+
 const apiKey='VVQQ2GWqnyH-u4U1ZMQJXND7NVhUq4puh-j4Dd9UAPkg2jSseZWkbmAZ_21CLKJvq_-zmYewBSfbz5NvQ810EAQpp_wgRNcktL5agbKNZ6PO5tW28LZad1TAWEC-XHYx';
 
 export const Yelp={
     search :(term, location, sortBy) =>{
-    return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, 
+    return fetch((`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`), 
         {
             headers: {
                 Authorization: `Bearer ${apiKey}`
@@ -27,6 +29,10 @@ export const Yelp={
                 };
             });
         }
+    }).then(function(businesses){
+        App.setState({
+            businesses: businesses
+        });
     });
 }
 }
