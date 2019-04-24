deddbca9be5ab5d6535fe24e60628a29b5cc2eac
spiritxhx <spiritxhx@gmail.com>
2019-04-23 21:09:33 -0400

    add client id

--- first-try/src/util/Yelp.js
+++ first-try/src/util/Yelp.js
@@ -1,5 +1,5 @@
 import App from "../App";
-
+const clientId='wTEduhbLx1cEdiaNNwg-zw';
 const apiKey='VVQQ2GWqnyH-u4U1ZMQJXND7NVhUq4puh-j4Dd9UAPkg2jSseZWkbmAZ_21CLKJvq_-zmYewBSfbz5NvQ810EAQpp_wgRNcktL5agbKNZ6PO5tW28LZad1TAWEC-XHYx';
 
 export const Yelp={
